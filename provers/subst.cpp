2fe42c71794e89d4a8684f6a31644ff5db0774c0
Giovanni Mascellani <gio@debian.org>
2018-03-16 12:13:51 +0100

    Implement substitution rules search.

--- provers/subst.cpp
+++ provers/subst.cpp
@@ -15,8 +15,8 @@ int subst_search_main(int argc, char *argv[]) {
 
     std::map< SymTok, std::pair< LabTok, LabTok > > equalities = {
         { tb.get_symbol("wff"), { tb.get_label("wb"), tb.get_label("biid") } },
-        { tb.get_symbol("set"), { tb.get_label("wceq"), tb.get_label("eqid") } },
-        { tb.get_symbol("class"), { tb.get_label(""), tb.get_label("") } },
+        { tb.get_symbol("class"), { tb.get_label("wceq"), tb.get_label("eqid") } },
+        //{ tb.get_symbol("set"), { tb.get_label(""), tb.get_label("") } },
     };
 
     // Find an equality symbol for each type
@@ -56,11 +56,7 @@ int subst_search_main(int argc, char *argv[]) {
             const auto label = der2.first;
             const auto &rule = der2.second;
             bool has_vars = false;
-            bool has_set_vars = false;
             for (const auto sym : rule) {
-                if (sym == tb.get_symbol("set")) {
-                    has_set_vars = true;
-                }
                 if (ders.find(sym) != ders.end()) {
                     has_vars = true;
                 }
@@ -68,9 +64,62 @@ int subst_search_main(int argc, char *argv[]) {
             if (!has_vars) {
                 continue;
             }
-            std::cout << "Considering derivation " << tb.resolve_label(label) <<" for type " << tb.resolve_symbol(type) << std::endl;
-            if (has_set_vars) {
-                std::cout << "  HAS SET VARS" << std::endl;
+            std::cout << "Considering derivation " << tb.resolve_label(label) <<" for type " << tb.resolve_symbol(type) << ", with variables:";
+            for (const auto sym : rule) {
+                if (ders.find(sym) != ders.end()) {
+                    std::cout << " " << tb.resolve_symbol(sym);
+                }
+            }
+            std::cout << std::endl;
+
+            for (unsigned i = 0; i < rule.size(); i++) {
+                auto it = equalities.find(rule[i]);
+                if (it != equalities.end()) {
+                    std::cout << " * Search for a substitution for " << tb.resolve_symbol(it->first) << " in position " << i << std::endl;
+                    tb.new_temp_var_frame();
+                    ParsingTree< SymTok, LabTok > pt_hyp;
+                    pt_hyp.label = it->second.first;
+                    pt_hyp.type = tb.get_turnstile_alias();
+                    ParsingTree< SymTok, LabTok > pt_left;
+                    ParsingTree< SymTok, LabTok > pt_right;
+                    pt_left.label = label;
+                    pt_left.type = type;
+                    pt_right.label = label;
+                    pt_right.type = type;
+                    for (unsigned j = 0; j < rule.size(); j++) {
+                        if (ders.find(rule[j]) != ders.end()) {
+                            ParsingTree< SymTok, LabTok > pt_var1;
+                            ParsingTree< SymTok, LabTok > pt_var2;
+                            auto var1 = tb.new_temp_var(rule[j]);
+                            pt_var1.label = var1.first;
+                            pt_var1.type = rule[j];
+                            if (i == j) {
+                                auto var2 = tb.new_temp_var(rule[j]);
+                                pt_var2.label = var2.first;
+                                pt_var2.type = rule[j];
+                                pt_hyp.children.push_back(pt_var1);
+                                pt_hyp.children.push_back(pt_var2);
+                            } else {
+                                pt_var2 = pt_var1;
+                            }
+                            pt_left.children.push_back(pt_var1);
+                            pt_right.children.push_back(pt_var2);
+                        }
+                    }
+                    ParsingTree< SymTok, LabTok > pt_thesis;
+                    pt_thesis.label = equalities[type].first;
+                    pt_thesis.type = tb.get_turnstile_alias();
+                    pt_thesis.children.push_back(pt_left);
+                    pt_thesis.children.push_back(pt_right);
+                    std::cout << "   Searching for " << tb.print_sentence(pt_thesis) << " with hypothesis " << tb.print_sentence(pt_hyp) << std::endl;
+                    auto res = tb.unify_assertion({std::make_pair(tb.get_turnstile(), pt_hyp)}, std::make_pair(tb.get_turnstile(), pt_thesis));
+                    if (!res.empty()) {
+                        std::cout << "   Found match " << tb.resolve_label(std::get<0>(res[0])) << std::endl;
+                    } else {
+                        std::cout << "   Found no match..." << std::endl;
+                    }
+                    tb.release_temp_var_frame();
+                }
             }
         }
     }
