de77772fbec8e7e22649d286b41bee11a42bf55f
Giovanni Mascellani <gio@debian.org>
2018-03-23 07:48:52 -0700

    Implement setting thread name on Windows.

--- platform.cpp
+++ platform.cpp
@@ -355,9 +355,38 @@ uint64_t platform_get_current_used_ram() {
     return pmc.WorkingSetSize;
 }
 
+// Wonderful clean API taken from https://msdn.microsoft.com/en-us/library/xcb2z8hs.aspx
+// Usage: SetThreadName ((DWORD)-1, "MainThread");
+#include <windows.h>
+const DWORD MS_VC_EXCEPTION = 0x406D1388;
+#pragma pack(push,8)
+typedef struct tagTHREADNAME_INFO
+{
+    DWORD dwType; // Must be 0x1000.
+    LPCSTR szName; // Pointer to name (in user addr space).
+    DWORD dwThreadID; // Thread ID (-1=caller thread).
+    DWORD dwFlags; // Reserved for future use, must be zero.
+ } THREADNAME_INFO;
+#pragma pack(pop)
+void SetThreadName(DWORD dwThreadID, const char* threadName) {
+    THREADNAME_INFO info;
+    info.dwType = 0x1000;
+    info.szName = threadName;
+    info.dwThreadID = dwThreadID;
+    info.dwFlags = 0;
+#pragma warning(push)
+#pragma warning(disable: 6320 6322)
+    __try{
+        RaiseException(MS_VC_EXCEPTION, 0, sizeof(info) / sizeof(ULONG_PTR), (ULONG_PTR*)&info);
+    }
+    __except (EXCEPTION_EXECUTE_HANDLER){
+    }
+#pragma warning(pop)
+}
+
 // It does not seem to be meaningful on Windows machines
 void platform_set_current_thread_name(const std::string &name) {
-    (void) name;
+    SetThreadName((DWORD) -1, name.c_str());
 }
 
 void platform_set_current_thread_low_priority() {
