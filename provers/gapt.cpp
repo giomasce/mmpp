54d1991d3fe1dd01632f6212afff811720be2e27
Giovanni Mascellani <gio@debian.org>
2019-05-14 22:28:46 +0200

    Implement other NDProof rules.

--- provers/gapt.cpp
+++ provers/gapt.cpp
@@ -144,6 +144,24 @@ private:
     proof subproof;
 };
 
+class AndElim1Rule : public NDProof, public gio::virtual_enable_create<AndElim1Rule> {
+protected:
+    AndElim1Rule(const sequent &thesis, const proof &subproof)
+        : NDProof(thesis), subproof(subproof) {}
+
+private:
+    proof subproof;
+};
+
+class AndElim2Rule : public NDProof, public gio::virtual_enable_create<AndElim2Rule> {
+protected:
+    AndElim2Rule(const sequent &thesis, const proof &subproof)
+        : NDProof(thesis), subproof(subproof) {}
+
+private:
+    proof subproof;
+};
+
 class ForallIntroRule : public NDProof, public gio::virtual_enable_create<ForallIntroRule> {
 protected:
     ForallIntroRule(const sequent &thesis, const std::shared_ptr<const gio::mmpp::provers::fof::Variable> &var, const std::shared_ptr<const gio::mmpp::provers::fof::Variable> &eigenvar, const proof &subproof)
@@ -161,6 +179,45 @@ protected:
 
 private:
     proof left_proof, right_proof;
+};
+
+class ImpElimRule : public NDProof, public gio::virtual_enable_create<ImpElimRule> {
+protected:
+    ImpElimRule(const sequent &thesis, const proof &left_proof, const proof &right_proof)
+        : NDProof(thesis), left_proof(left_proof), right_proof(right_proof) {}
+
+private:
+    proof left_proof, right_proof;
+};
+
+class AndIntroRule : public NDProof, public gio::virtual_enable_create<AndIntroRule> {
+protected:
+    AndIntroRule(const sequent &thesis, const proof &left_proof, const proof &right_proof)
+        : NDProof(thesis), left_proof(left_proof), right_proof(right_proof) {}
+
+private:
+    proof left_proof, right_proof;
+};
+
+class ExistsElimRule : public NDProof, public gio::virtual_enable_create<ExistsElimRule> {
+protected:
+    ExistsElimRule(const sequent &thesis, ssize_t idx, const std::shared_ptr<const gio::mmpp::provers::fof::Variable> &eigenvar, const proof &left_proof, const proof &right_proof)
+        : NDProof(thesis), idx(idx), eigenvar(eigenvar), left_proof(left_proof), right_proof(right_proof) {}
+
+private:
+    ssize_t idx;
+    std::shared_ptr<const gio::mmpp::provers::fof::Variable> eigenvar;
+    proof left_proof, right_proof;
+};
+
+class ContractionRule : public NDProof, public gio::virtual_enable_create<ContractionRule> {
+protected:
+    ContractionRule(const sequent &thesis, ssize_t idx1, ssize_t idx2, const proof &subproof)
+        : NDProof(thesis), idx1(idx1), idx2(idx2), subproof(subproof) {}
+
+private:
+    ssize_t idx1, idx2;
+    proof subproof;
 };
 
 class ExcludedMiddleRule : public NDProof, public gio::virtual_enable_create<ExcludedMiddleRule> {
@@ -183,10 +240,37 @@ std::shared_ptr<const NDProof> parse_gapt_proof(std::istream &is) {
         auto left_proof = parse_gapt_proof(is);
         auto right_proof = parse_gapt_proof(is);
         return ExcludedMiddleRule::create(thesis, left_idx, right_idx, left_proof, right_proof);
+    } else if (type == "Contraction") {
+        ssize_t idx1, idx2;
+        is >> idx1 >> idx2;
+        auto subproof = parse_gapt_proof(is);
+        return ContractionRule::create(thesis, idx1, idx2, subproof);
+    } else if (type == "AndElim1") {
+        auto subproof = parse_gapt_proof(is);
+        return AndElim1Rule::create(thesis, subproof);
+    } else if (type == "AndElim2") {
+        auto subproof = parse_gapt_proof(is);
+        return AndElim2Rule::create(thesis, subproof);
     } else if (type == "NegElim") {
         auto left_proof = parse_gapt_proof(is);
         auto right_proof = parse_gapt_proof(is);
         return NegElimRule::create(thesis, left_proof, right_proof);
+    } else if (type == "AndIntro") {
+        auto left_proof = parse_gapt_proof(is);
+        auto right_proof = parse_gapt_proof(is);
+        return AndIntroRule::create(thesis, left_proof, right_proof);
+    } else if (type == "ExistsElim") {
+        ssize_t idx;
+        is >> idx;
+        auto eigenvar = std::dynamic_pointer_cast<const gio::mmpp::provers::fof::Variable>(parse_gapt_formula(is));
+        gio::assert_or_throw<std::invalid_argument>(bool(eigenvar), "missing eigenvariable");
+        auto left_proof = parse_gapt_proof(is);
+        auto right_proof = parse_gapt_proof(is);
+        return ExistsElimRule::create(thesis, idx, eigenvar, left_proof, right_proof);
+    } else if (type == "ImpElim") {
+        auto left_proof = parse_gapt_proof(is);
+        auto right_proof = parse_gapt_proof(is);
+        return ImpElimRule::create(thesis, left_proof, right_proof);
     } else if (type == "LogicalAxiom") {
         auto form = parse_gapt_formula(is);
         return LogicalAxiom::create(thesis, form);
@@ -214,7 +298,7 @@ std::shared_ptr<const NDProof> parse_gapt_proof(std::istream &is) {
         auto var = std::dynamic_pointer_cast<const gio::mmpp::provers::fof::Variable>(parse_gapt_formula(is));
         gio::assert_or_throw<std::invalid_argument>(bool(var), "missing quantified variable");
         auto eigenvar = std::dynamic_pointer_cast<const gio::mmpp::provers::fof::Variable>(parse_gapt_formula(is));
-        gio::assert_or_throw<std::invalid_argument>(bool(var), "missing eigenvariable");
+        gio::assert_or_throw<std::invalid_argument>(bool(eigenvar), "missing eigenvariable");
         auto subproof = parse_gapt_proof(is);
         return ForallIntroRule::create(thesis, var, eigenvar, subproof);
     } else {
